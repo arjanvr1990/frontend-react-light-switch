a7b9a6a44370fd33318a08985e62c73998a6f3f1
arjanvr1990 <arjanvanraamsdonk@gmail.com>
2023-09-12 16:12:55 +0200

    opdracht gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const [dot, setDot] = React.useState(" ");
+
+
     return (
-        <main className="off">
+        <main className={dot === "on" ? "on" : "off"}>
             <section>
                 <div className="dot"></div>
-                <button type="button">Turn on/off</button>
+                <button
+                    type="button"
+                    onClick={() => setDot (turnOnOff => (turnOnOff === "on" ? "off" : "on")) }>Turn {dot === "on" ? "off" : "on"}</button>
             </section>
         </main>
     );
